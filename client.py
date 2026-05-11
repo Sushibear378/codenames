a6d28a145ff30ba425a6a34eac2de71658f01df0
Kai Lin <kai.lin@W5004823.ca0375.muenchen.musin.de>
2026-05-11 08:55:26 +0200

    11.05 III.1

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 # client.py (run this on each client device)
 import socket
 
-server_ip = 'YOUR_COMPUTER_IP'  # e.g., '192.168.1.100'
+server_ip = '10.97.36.101'  # e.g., '192.168.1.100'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((server_ip, 5000))
 client.sendall(b'Hello from client')
